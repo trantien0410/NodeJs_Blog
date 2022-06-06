a8e69e69f87ab3d3f48d55f4967499dfe5d352cb
tien04100 <tien04100@users.noreply.github.com>
2022-06-06 15:40:38 +0700

    count delete documents

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,15 +5,28 @@ const Course = require("../models/Course.");
 class MeController {
     //[GET] /me/stored/musics
     storedMusics(req, res, next) {
-        Course.find({})
-        .then(courses => res.render("me/stored-musics",{courses}))
-        .catch(next);
+
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deleteCount])=>res.render("me/stored-musics", {
+                deleteCount, courses })
+            )
+            .catch(next);
+
+        // Course.countDocumentsDeleted()
+        //   .then((deleteCount) => {
+        //     console.log(deleteCount);
+        //   })
+        //   .catch(() => {});
+
+        // Course.find({})
+        //   .then((courses) => res.render("me/stored-musics", { courses }))
+        //   .catch(next);
     }
 
     //[GET] /me/trashbin/musics
     trashbinMusics(req, res, next) {
         Course.findDeleted({})
-        .then(courses => res.render("me/trashbin-musics",{courses}))
+        .then((courses) => res.render("me/trashbin-musics",{courses}))
         .catch(next);
     }
 }
